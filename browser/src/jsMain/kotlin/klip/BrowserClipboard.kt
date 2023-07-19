8c07e4e3246a39890dcf348892b6680bcf962970
andylamax <andylamax@programmer.net>
2023-07-19 06:38:37 +0300

    Added klip for clipboard purposes

--- browser/src/jsMain/kotlin/klip/BrowserClipboard.kt
+++ browser/src/jsMain/kotlin/klip/BrowserClipboard.kt
@@ -7,6 +7,7 @@ import kotlinx.browser.window
 class BrowserClipboard : AbstractClipboard() {
 
     val clipboard = window.navigator.clipboard
+
     override fun data() = clipboard.readText().then { TextClipData(it) }.asLater()
 
     override fun set(data: ClipData) = clipboard.writeText(data.asText.content).asLater()
