53591f95bc760753479c9313bfe003a65b8153e0
andylamax <andylamax@programmer.net>
2023-12-24 12:48:14 +0300

    Massive update welcoming K2

--- local/src/commonMain/kotlin/klip/LocalClipboard.kt
+++ local/src/commonMain/kotlin/klip/LocalClipboard.kt
@@ -3,6 +3,11 @@ package klip
 import klip.internal.AbstractClipboard
 import koncurrent.FailedLater
 import koncurrent.Later
+import koncurrent.later.then
+import koncurrent.later.andThen
+import koncurrent.later.andZip
+import koncurrent.later.zip
+import koncurrent.later.catch
 
 class LocalClipboard : AbstractClipboard() {
     private var content: ClipData? = null
